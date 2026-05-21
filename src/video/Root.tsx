c60fc13173aefabf6affa8050b5c4d146b718fcb
joshuaweingarten <91756719+joshuaweingarten@users.noreply.github.com>
2026-05-21 09:44:09 -0400

    Register pro video composition

--- src/video/Root.tsx
+++ src/video/Root.tsx
@@ -1,5 +1,6 @@
 import { Composition } from "remotion";
 import { CouldaMadeFinanceVideo } from "./templates/CouldaMadeFinanceVideo";
+import { CouldaMadeProVideo } from "./templates/CouldaMadeProVideo";
 import { videoInputSchema } from "../shared/types";
 
 export const VIDEO_FPS = 30;
@@ -9,15 +10,27 @@ export const VIDEO_DURATION_FRAMES = 22 * VIDEO_FPS;
 
 export function RemotionRoot() {
   return (
-    <Composition
-      id="CouldaMadeFinance"
-      component={CouldaMadeFinanceVideo}
-      durationInFrames={VIDEO_DURATION_FRAMES}
-      fps={VIDEO_FPS}
-      width={VIDEO_WIDTH}
-      height={VIDEO_HEIGHT}
-      schema={videoInputSchema}
-      defaultProps={videoInputSchema.parse({})}
-    />
+    <>
+      <Composition
+        id="CouldaMadeFinance"
+        component={CouldaMadeFinanceVideo}
+        durationInFrames={VIDEO_DURATION_FRAMES}
+        fps={VIDEO_FPS}
+        width={VIDEO_WIDTH}
+        height={VIDEO_HEIGHT}
+        schema={videoInputSchema}
+        defaultProps={videoInputSchema.parse({})}
+      />
+      <Composition
+        id="CouldaMadePro"
+        component={CouldaMadeProVideo}
+        durationInFrames={VIDEO_DURATION_FRAMES}
+        fps={VIDEO_FPS}
+        width={VIDEO_WIDTH}
+        height={VIDEO_HEIGHT}
+        schema={videoInputSchema}
+        defaultProps={videoInputSchema.parse({ template: "couldamade-pro" })}
+      />
+    </>
   );
 }
